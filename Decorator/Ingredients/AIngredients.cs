d689cfee942ba1241b4de5443f538f178d363c2d
werwolfy <raphael.nigl@outlook.de>
2026-03-12 10:26:07 +0100

    [TASK] Removed unnecessary GetIngredientPrice

--- Decorator/Ingredients/AIngredients.cs
+++ Decorator/Ingredients/AIngredients.cs
@@ -7,9 +7,6 @@ public abstract class AIngredients : IBurgerIngredients
 {
     public IBurgerIngredients? Ingredients { get; set; }
     public double Price { get; set; }
-    
-    public virtual double GetIngredientPrice()
-    {
-        throw new NotImplementedException();
-    }
+
+    public abstract double GetIngredientPrice();
 }
